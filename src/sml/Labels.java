14ccf673751812e8c4d84a64f1a3b03d752c1312
dimgak <gakidisd@gmail.com>
2023-02-20 15:08:16 +0000

    - Labels.java > JavaDoc 1.0 v

--- src/sml/Labels.java
+++ src/sml/Labels.java
@@ -8,7 +8,8 @@ import java.util.stream.Collectors;
 // TODO: write a JavaDoc for the class
 
 /**
- *
+ * 1.0v
+ * Represents the "dictionary" of the labels where each label points to an address to the memory.
  * @author gakid
  */
 public final class Labels {
